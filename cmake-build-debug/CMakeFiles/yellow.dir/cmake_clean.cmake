30381f1e0de2fbc6d6b2d0681fb8732090ca4ddc
Igor Bulakh <bulakhigor@yandex-team.ru>
2019-02-17 00:11:40 +0300

    Fixed all things, that have been commented. Also, I have found that lots of exaptions if I change [] to .at

---
+++ cmake-build-debug/CMakeFiles/yellow.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yellow.dir/main.cpp.o"
+  "yellow.pdb"
+  "yellow"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yellow.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
